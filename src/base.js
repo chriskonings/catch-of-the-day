8e6e374508018be7088930aada7e22a0a25b51de
Chris Konings <chriskonings@CK.local>
2017-03-16 16:13:02 -0400

    first commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCBBQIFQBW3zu0xlOcnKmCd9qstnOTIduM",
+  authDomain: "catch-of-the-day-chris-konings.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-chris-konings.firebaseio.com",
+});
+
+export default base;
